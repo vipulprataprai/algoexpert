eda04a868fd974b30c02c3046db70f85fa6112e1
vipulprataprai <vipulprataprai@gmail.com>
2023-01-15 00:39:26 -0800

    Step1 of Learing git: Added comment1 to the code

--- recursion/product_sum.py
+++ recursion/product_sum.py
@@ -1,6 +1,8 @@
 #Complexity :: Time = O(n) Space = O(d) {d is the greatest depth of the special array in the array} 
 def productSum(array, multiplier =1):
     # Write your code here.
+    # I am leating git
+    #Cheers to learning. First step is important.
 	sum = 0
 	for element in array:
 		if type(element) is list:
