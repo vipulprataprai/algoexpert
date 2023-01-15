df3517eafac1a6885325bd72ff47e13364ac6c7d
vipulprataprai <vipulprataprai@gmail.com>
2023-01-15 00:46:51 -0800

    Step2 of learning. Excited to continue

--- recursion/product_sum.py
+++ recursion/product_sum.py
@@ -1,7 +1,9 @@
 #Complexity :: Time = O(n) Space = O(d) {d is the greatest depth of the special array in the array} 
 def productSum(array, multiplier =1):
     # Write your code here.
-	sum = 0
+    #Step 2 line :: Learning is going to be awesome.
+    #Practice is a good way of learning.
+     	sum = 0
 	for element in array:
 		if type(element) is list:
 			sum += productSum(element, multiplier + 1)
